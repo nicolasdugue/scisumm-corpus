78c9ee132eff4450476af9900d337aee07e8d859
Nicolas Dugué <dugue.nic@gmail.com>
2016-03-31 12:14:30 +0200

    Improving class and creating test file

--- code/pyfmax/fmax.py
+++ code/pyfmax/fmax.py
@@ -1,26 +1,53 @@
-class fmax:
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-	def __init__(self, matrix_csr, matrix_csc, clustering, labels_line=[], labels_col=[]):
+from scipy.sparse import csr_matrix, csc_matrix
 
-		self.matrix_csr = matrix_csr
+class MatrixClustered:
+	"""
+		This class allows to define a matrix which rows (objects) were clustered
+		Labels can be used to describe rows (objects) and columns (features)
+    	"""
 
-		self.matrix_csc = matrix_csc
+	def __init__(self, matrix, clustering, labels_row=[], labels_col=[]):
+		"""
+		Matrix and clustering should be at least passed.
+		Matrix should be a 2D Array or already a sparse (csr or csc) matrix.
+		Clustering should be an array where a value v at index i defines that the object i (row i in matrix) belongs to cluster v
+		Labels can be used to describe rows (objects) and columns (features) in the same way as the clustering object
+    		"""
+
+		self.matrix_csr = csr_matrix(matrix)
+
+		self.matrix_csc = csc_matrix(matrix)
 
 		self.clustering=clustering
 
-		self.labels_line=labels_line
+		self.labels_row=labels_row
 
 		self.labels_col=labels_col
 
-		self.sum_rows=matrix_csr.sum(axis=1)
+		self.sum_rows=self.matrix_csr.sum(axis=1)
 
-		self.sum_cols=matrix_csc.sum(axis=0)
+		self.sum_cols=self.matrix_csc.sum(axis=0)
 
 		
 
-	def sum_row(i):
+	def sum_row(self, i):
+		"""
+		Get the sum of row i
+    		"""
 		return self.sum_rows[i]
 
-	def sum_col(i):
+	def sum_col(self, i):
+		"""
+		Get the sum of column i
+    		"""
 		return self.sum_cols[i]
+
+	def __str__(self):
+		"""
+		toString()
+    		"""
+		return "Matrix CSR (ordered by rows) :\n" + str(self.matrix_csr)+ "\nMatrix CSC (ordered by columns): \n"+ str(self.matrix_csc) + "\nColumns labels (features) " + str(self.labels_col) + "\nRows labels (objects) " + str(self.labels_row) + "\nClusters " + str(self.clustering)
 		
---
+++ code/test_isko.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Include the fmax file of package pyfmax using fm namespace
+import pyfmax.fmax as fm
+#import numpy lib using np namespace
+import numpy as np
+
+#Load the file as a 1D array
+clustering=np.loadtxt("exemple_isko/clustering_isko")
+labels_col=[]
+#Read the labels of columns (features)
+labels=open("exemple_isko/label_isko")
+for ligne in labels:
+	labels_col.append(ligne.strip())
+#Load the file as a 2D array
+matrix=np.loadtxt("exemple_isko/matrix_isko")
+
+#Create a MatrixClustered object using fm namespace which refers to fmax.py in package pyfmax
+obj=fm.MatrixClustered(matrix, clustering,labels_col=labels_col)
+print obj
